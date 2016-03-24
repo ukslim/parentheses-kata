fdb37c4c10dfd370e101cebc808c239770eaf8b0
John Hartnup <john.hartnup@firstutility.com>
2016-03-24 16:16:09 +0000

    Maven init

---
+++ src/main/java/com/github/ukslim/Checker.java
@@ -0,0 +1,9 @@
+package com.github.ukslim;
+
+public class Checker 
+{
+    public int countSubstrings(String args)
+    {
+        return 2;
+    }
+}
---
+++ src/test/java/com/github/ukslim/CheckerTest.java
@@ -0,0 +1,12 @@
+package com.github.ukslim;
+
+import org.junit.Test;
+
+public class CheckerTest 
+{
+
+    @Test
+    public void testChecker()
+    {
+    }
+}
