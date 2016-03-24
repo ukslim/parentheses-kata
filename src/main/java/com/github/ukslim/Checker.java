ded05fcab2e3e142bb990a3a161351adbfccb36f
John Hartnup <john.hartnup@firstutility.com>
2016-03-24 17:41:51 +0000

    Possibly naive implementation

--- src/main/java/com/github/ukslim/Checker.java
+++ src/main/java/com/github/ukslim/Checker.java
@@ -1,9 +1,41 @@
 package com.github.ukslim;
 
-public class Checker 
+import java.util.function.BiConsumer;
+
+public class Checker
 {
-    public int countSubstrings(String args)
+    public void findSubstrings(String s, BiConsumer<Integer,Integer> handler) 
     {
-        return 2;
+        bal(s.toCharArray(), handler);
+    }
+    
+    private void bal(char[] chars, BiConsumer<Integer,Integer> handler) {
+        for(int i=0; i<chars.length; i++) {
+            findClosesAtThisLevel(chars, i, handler);
+        }
+    }
+    
+    private void findClosesAtThisLevel(char[] chars, int pos, BiConsumer<Integer,Integer> handler) {
+        if(chars[pos] == ')') {
+            return;
+        } else {
+            int level = 0;
+            for(int i = pos; i<chars.length; i++) {
+                if(chars[i] == '(') {
+                    level++;
+                }
+                if(chars[i] == ')') {
+                    level--;
+                    if(level == 0) {
+                        handler.accept(pos, i);
+                    }
+                    if(level < 0) {
+                        // reached beyond scope
+                        break; 
+                    }
+                }
+            }
+
+        }
     }
 }
--- src/test/java/com/github/ukslim/CheckerTest.java
+++ src/test/java/com/github/ukslim/CheckerTest.java
@@ -1,12 +1,99 @@
 package com.github.ukslim;
 
-import org.junit.Test;
+import java.util.*;
+import java.util.function.BiConsumer;
+import org.junit.*;
 
-public class CheckerTest 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class CheckerTest
 {
-
+    
+    Checker checker = new Checker();
+    int count;
+    BiConsumer<Integer, Integer> addToCount = ( start, end ) -> {
+        count ++;
+    };
+    
+    @Before
+    public void resetCount() {
+        count = 0;
+    }
+    
     @Test
-    public void testChecker()
-    {
+    public void simplest_case_is_1() {
+        checker.findSubstrings("()", addToCount);
+        assertEquals(1, count);
+    }
+    
+    @Test
+    public void two_in_a_row_is_3() {
+        checker.findSubstrings("()()", addToCount);
+        assertEquals(3, count);
+    }
+    
+    @Test
+    public void three_in_a_row_is_6() {
+        checker.findSubstrings("()()()", addToCount);
+        assertEquals(6, count);
+    }
+    
+    @Test
+    public void two_nested_is_2() {
+        checker.findSubstrings("(())", addToCount);
+        assertEquals(2, count);
+    }
+    
+    @Test
+    public void three_nested_is_3() {
+        checker.findSubstrings("((()))", addToCount);
+        assertEquals(3, count);
+    }
+    
+    @Test
+    public void complex_case() {
+        checker.findSubstrings("(()((()()(())()))", addToCount);
+        assertEquals(15, count);
+    }
+    
+    @Test
+    public void check_results() {
+        final String input = "(()((()()(())())))";
+        List<String> substrings = new ArrayList<>();
+        BiConsumer<Integer,Integer> handler = (start,end) -> {
+            substrings.add(makeDisplayString(input, start, end));
+        };
+        
+        checker.findSubstrings(input, handler);
+        
+        List<String> expected = Arrays.asList(
+                "(()((()()(())())))" ,
+                " ()               " ,
+                " ()((()()(())())) " ,
+                "   ((()()(())())) " ,
+                "    (()()(())())  " ,
+                "     ()           " ,
+                "     ()()         " ,
+                "     ()()(())     " ,
+                "     ()()(())()   " ,
+                "       ()         " ,
+                "       ()(())     " ,
+                "       ()(())()   " ,
+                "         (())     " ,
+                "         (())()   " ,
+                "          ()      " ,
+                "             ()   ");
+        assertThat(substrings, is(expected));
+    }
+
+    private String makeDisplayString(String input, Integer start, Integer end) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            s.append((i>=start && i<=end ) ? input.charAt(i) : ' ');
+        }
+        final String displayString = s.toString();
+        return displayString;
     }
 }
