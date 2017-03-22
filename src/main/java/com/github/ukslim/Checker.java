71a628f209a341900ebb26c70848f1e98ca8388a
John Hartnup <john.hartnup@firstutility.com>
2017-03-22 16:06:14 +0000

    Alternative solution

--- src/main/java/com/github/ukslim/Checker.java
+++ src/main/java/com/github/ukslim/Checker.java
@@ -1,41 +1,52 @@
 package com.github.ukslim;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class Checker
-{
-    public void findSubstrings(String s, BiConsumer<Integer,Integer> handler) 
-    {
+public class Checker {
+
+    private static final char OPEN = '(';
+    private static final char CLOSE = ')';
+
+    public void findSubstrings(String s, BiConsumer<Integer, Integer> handler) {
         bal(s.toCharArray(), handler);
     }
-    
-    private void bal(char[] chars, BiConsumer<Integer,Integer> handler) {
-        for(int i=0; i<chars.length; i++) {
-            findClosesAtThisLevel(chars, i, handler);
-        }
-    }
-    
-    private void findClosesAtThisLevel(char[] chars, int pos, BiConsumer<Integer,Integer> handler) {
-        if(chars[pos] == ')') {
-            return;
-        } else {
-            int level = 0;
-            for(int i = pos; i<chars.length; i++) {
-                if(chars[i] == '(') {
-                    level++;
-                }
-                if(chars[i] == ')') {
-                    level--;
-                    if(level == 0) {
-                        handler.accept(pos, i);
-                    }
-                    if(level < 0) {
-                        // reached beyond scope
-                        break; 
-                    }
-                }
-            }
 
+    private void bal(char[] chars, BiConsumer<Integer, Integer> handler) {
+
+        Map<Integer, List<Integer>> starts = new HashMap<>();
+
+        int level = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == OPEN) {
+                recordOpen(starts, i, ++level);
+            } else if (c == CLOSE) {
+                handleClose(starts, i, level--, handler);
+            }
         }
     }
+
+    private void recordOpen(Map<Integer, List<Integer>> starts, int pos, int level) {
+
+        starts.merge(level, Collections.singletonList(pos),
+                (a, b) -> Stream.concat(a.stream(), b.stream()).collect(Collectors.toList()));
+
+    }
+
+    private void handleClose(Map<Integer, List<Integer>> starts, int closePos, int level,
+            BiConsumer<Integer, Integer> handler) {
+        List<Integer> startPositions = starts.get(level);
+        startPositions.forEach((start) -> {
+            handler.accept(start, closePos);
+        });
+
+    }
+
 }
